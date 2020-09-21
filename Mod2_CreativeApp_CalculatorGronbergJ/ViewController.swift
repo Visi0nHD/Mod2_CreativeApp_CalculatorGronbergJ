f364c6fb49c2517673d988bcdeaa48184da3d8e7
Jack Gronberg <Jackgronberg@gmail.com>
2020-09-21 08:51:12 -0500

    Initial Commit

---
+++ Mod2_CreativeApp_CalculatorGronbergJ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mod2_CreativeApp_CalculatorGronbergJ
+//
+//  Created by Tiger Coder on 9/21/20.
+//  Copyright © 2020 Jack Gronberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
