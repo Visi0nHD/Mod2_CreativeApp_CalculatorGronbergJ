cb3b6e3e21b9d54341aa7517c6dad5e6a7d4a663
Jack Gronberg <Jackgronberg@gmail.com>
2020-09-21 09:54:40 -0500

    nice

--- Mod2_CreativeApp_CalculatorGronbergJ/ViewController.swift
+++ Mod2_CreativeApp_CalculatorGronbergJ/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var aField: UITextField!
+    @IBOutlet weak var bField: UITextField!
+    @IBOutlet weak var cField: UITextField!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    var a = 0
+    var b = 0
+    var c = 0
+    @IBAction func resetButton(_ sender: Any) {
+        
+    }
+   
+    @IBAction func calculateButton(_ sender: Any) {
+        let strA = aField.text!
+        a = Int(strA) ?? 0
+        let strB = bField.text!
+        b = Int(strB) ?? 0
+        let strC = cField.text!
+        c = Int(strC) ?? 0
+        if(c==0){
+        a = a * a
+        b = b * b
+        c = a + b
+            answerLabel.text = String("\(c)")
+        }
+    }
 
 }
-
